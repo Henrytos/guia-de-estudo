bdd0e198cf00a22591bb8e5e4e92d90eaa77945a
Henry <franzhenry46@gmail.com>
2025-01-13 17:42:35 -0300

    projeto semana 1

--- semana-1/test.ts
+++ semana-1/test.ts
@@ -13,6 +13,16 @@ function OmitValues<Type extends Object, Keys extends keyof Type>(
   return valueValid as Omit<Type, Keys>;
 }
 
+const user = {
+  name: "heney",
+  email: "henry@gmail.com",
+  password: "123456",
+};
+
+const userWithoutPassword = OmitValues(user, "password");
+
+console.log(userWithoutPassword); // { name: 'heney', email: 'henry@gmail.com' }
+
 function PickValues<Type extends object, Keys extends keyof Type>(
   value: Type,
   ...keys: Keys[]
@@ -28,15 +38,6 @@ function PickValues<Type extends object, Keys extends keyof Type>(
   return valueValid as Pick<Type, Keys>;
 }
 
-const user = {
-  name: "heney",
-  email: "henry@gmail.com",
-  password: "123456",
-};
-
-const userWithoutPassword = OmitValues(user, "password");
-
 const userWithPassword = PickValues(user, "password");
 
-console.log(userWithoutPassword);
-console.log(userWithPassword);
+console.log(userWithPassword); // { password: '123456' }
