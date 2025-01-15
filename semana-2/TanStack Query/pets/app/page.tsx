c8f568243b1722dab4dcc05ce9c6c017693ab5cb
Henry <franzhenry46@gmail.com>
2025-01-15 18:14:20 -0300

    utilizando autenticação com tanstack query

--- semana-2/TanStack Query/pets/app/page.tsx
+++ semana-2/TanStack Query/pets/app/page.tsx
@@ -1,25 +1,66 @@
 "use client";
 
+import { authenticate } from "@/api/authenticate";
 import { fetchPets } from "@/api/fetch-pets";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
   const { data, isPending, isError } = useQuery({
     queryKey: ["pets"],
     queryFn: () => fetchPets({}),
-    refetchOnWindowFocus: true,
   });
 
-  const mutation = useMutation({
-    mutationFn:
-  })
+  const { mutate } = useMutation({
+    mutationFn: authenticate,
+    onSuccess: () => {
+      console.log("sucesso");
+    },
+    onError: () => {
+      console.log("error");
+    },
+  });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(window.Storage);
+
+  const submitForm = (ev: FormEvent) => {
+    ev.preventDefault();
+    mutate({ email, password });
+  };
   return (
     <main className="bg-zinc-900 text-zinc-100 w-full min-h-svh px-10 py-20 ">
       <div className="max-w-6xl m-auto">
         <h1 className="text-2xl font-semibold before:h-6 before:w-1 before:bg-zinc-100 before:block before:absolute before:left-0 before:top-1 relative pl-3 pb-5">
           Pets
         </h1>
+
+        <form onSubmit={submitForm} className=" space-x-2">
+          <input
+            name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="example@gmai.com"
+            className="border-1 border-solid border-zinc-100 bg-zinc-900 text-zinc-300 font-light text-sm "
+          />
+
+          <input
+            name="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="password"
+            className="border-1 border-solid border-zinc-100 bg-zinc-900 text-zinc-300 font-light text-sm "
+          />
+
+          <button>enviar </button>
+        </form>
 
         <hr className="mb-10" />
 
